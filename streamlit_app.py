1745c8a483ad3313aee767528d6a2593e79ce7ac
noaccours <156504045+noaccours@users.noreply.github.com>
2024-02-25 10:31:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from os.path import exists
+import telebot
 
 # Set the desired width and height
 width = 1000
 height = 600
+
+# Initialize your Telegram bot
+bot = telebot.TeleBot("6148264758:AAHCAU_v3P9SAkCZLPuS6sStZ9Fhbjskp_w")
 
 def get_driver():
     options = webdriver.ChromeOptions()
@@ -37,7 +41,29 @@ def get_screenshot(app_url):
     else:
         driver.get(app_url)
 
+#time.sleep(3)
+    
+    # Capture the screenshot
+    screenshot_path = 'screenshot.png'
+    driver.save_screenshot(screenshot_path)
+    
+    # Close the driver
+    driver.quit()
+    
+    return screenshot_path
+
 # Example usage
-app_url = 'https://facebook.com'  
-print("success")
-get_screenshot(app_url)
+app_url = 'https://facebook.com'  # Set the URL you want to capture
+print("Success")
+screenshot_path = get_screenshot(app_url)
+
+# Sending the screenshot to Telegram
+@bot.message_handler(commands=['ss'])
+def send_screenshot(message):
+    chat_id = message.chat.id
+    with open(screenshot_path, 'rb') as photo:
+        bot.send_photo(chat_id, photo)
+
+# Start the bot
+bot.polling()
+
